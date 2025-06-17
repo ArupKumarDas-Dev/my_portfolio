584db378f9d6014154f22530e16014fe16f06dd6
Arup_kr_das <153302802+ArupKumarDas-Dev@users.noreply.github.com>
2025-06-17 10:00:25 +0530

    Update script.js for preloader

--- script.js
+++ script.js
@@ -33,3 +33,13 @@ var sidemeu = document.getElementById("sidemenu");
   function closemenu() {
     sidemeu.style.right = "-200px";
   }
+
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  preloader.style.transition = "opacity 0.5s ease";
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 500);
+});
+
